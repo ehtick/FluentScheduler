2b804c922f2c80d7015836ba73f95e4b3c6d75e2
Talles L <talleslasmar@gmail.com>
2025-08-02 17:52:58 -0300

    Fixed IDE0078 (Use pattern matching).

--- FluentScheduler/Fluent/3.Duration/PeriodOnceSet.cs
+++ FluentScheduler/Fluent/3.Duration/PeriodOnceSet.cs
@@ -18,10 +18,10 @@ public class PeriodOnceSet
     /// <param name="minute">The minutes (0 through 59).</param>
     public void At(int hour, int minute)
     {
-        if (hour < 0 || hour > 23)
+        if (hour is < 0 or > 23)
             throw new ArgumentOutOfRangeException($"\"{nameof(hour)}\" should be in the 0 to 23 range.");
 
-        if (minute < 0 || minute > 59)
+        if (minute is < 0 or > 59)
             throw new ArgumentOutOfRangeException($"\"{nameof(minute)}\" should be in the 0 to 59 range.");
 
         _calculator.PeriodCalculations.Add(last => new DateTime(last.Year, last.Month, last.Day, hour, minute, 0));
