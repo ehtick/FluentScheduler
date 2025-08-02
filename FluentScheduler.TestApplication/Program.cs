dae0d61abaa1de4cf4c38569176b23ce71737d01
Talles L <talleslasmar@gmail.com>
2025-08-02 20:35:40 -0300

    Fixed IDE0040 (Add accessibility modifiers).

--- FluentScheduler.TestApplication/Program.cs
+++ FluentScheduler.TestApplication/Program.cs
@@ -7,7 +7,7 @@ using static System.Threading.Thread;
 
 internal static class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
         InitializeLogger();
 
@@ -39,13 +39,12 @@ internal static class Program
         Sleep(-1);
     }
 
-
-    static Schedule Welcome() => new Schedule(
+    private static Schedule Welcome() => new Schedule(
         () => Logger.Information("3, 2, 1, live!"),
         run => run.Now()
     );
 
-    static Schedule NonReentrant() => new Schedule(() =>
+    private static Schedule NonReentrant() => new Schedule(() =>
         {
             Logger.Information("NonReentrant: sleeping a minute");
             Sleep(TimeSpan.FromMinutes(1));
@@ -53,7 +52,7 @@ internal static class Program
         run => run.Every(1).Seconds()
     );
 
-    static void InitializeLogger()
+    private static void InitializeLogger()
     {
         var outputTemplate = "[{Timestamp:HH:mm:ss}] {Message}{NewLine}";
 
@@ -64,74 +63,74 @@ internal static class Program
             .CreateLogger();
     }
 
-    static Schedule Faulty() => new Schedule(
+    private static Schedule Faulty() => new Schedule(
         () => { throw new InvalidOperationException("Exception from faulty job"); },
         run => run.Now().AndEvery(20).Minutes()
     );
 
-    static Schedule FiveMinutes() => new Schedule(
+    private static Schedule FiveMinutes() => new Schedule(
         () => Logger.Information("FiveMinutes: five minutes has passed"),
         run => run.OnceAt(DateTime.Now.AddMinutes(5)).AndEvery(5).Minutes()
     );
 
-    static Schedule TenMinutes() => new Schedule(
+    private static Schedule TenMinutes() => new Schedule(
         () => Logger.Information("TenMinutes: ten minutes has passed"),
         run => run.Every(10).Minutes()
     );
 
-    static Schedule Hour() => new Schedule(
+    private static Schedule Hour() => new Schedule(
         () => Logger.Information("Hour: a hour has passed"),
         run => run.Every(1).Hours()
     );
 
-    static Schedule Day() => new Schedule(
+    private static Schedule Day() => new Schedule(
         () => Logger.Information("Day: a day has passed"),
         run => run.Every(1).Days()
     );
 
-    static Schedule Weekday() => new Schedule(
+    private static Schedule Weekday() => new Schedule(
         () => Logger.Information("Weekday: a new weekday has started"),
         run => run.EveryWeekday()
     );
 
-    static Schedule Sunday() => new Schedule(
+    private static Schedule Sunday() => new Schedule(
         () => Logger.Information("Sunday: a new sunday has started "),
         run => run.Every(DayOfWeek.Sunday)
     );
 
-    static Schedule Monday() => new Schedule(
+    private static Schedule Monday() => new Schedule(
         () => Logger.Information("Monday: a new monday has started "),
         run => run.Every(DayOfWeek.Monday)
     );
 
-    static Schedule Tuesday() => new Schedule(
+    private static Schedule Tuesday() => new Schedule(
         () => Logger.Information("Tuesday: a new tuesday has started "),
         run => run.Every(DayOfWeek.Tuesday)
     );
 
-    static Schedule Thursday() => new Schedule(
+    private static Schedule Thursday() => new Schedule(
         () => Logger.Information("Thursday: a new thursday has started "),
         run => run.Every(DayOfWeek.Thursday)
     );
 
-    static Schedule Friday() => new Schedule(
+    private static Schedule Friday() => new Schedule(
         () => Logger.Information("Friday: a new friday has started "),
         run => run.Every(DayOfWeek.Friday)
     );
 
-    static Schedule Saturday() => new Schedule(
+    private static Schedule Saturday() => new Schedule(
         () => Logger.Information("Saturday: a new saturday has started "),
         run => run.Every(DayOfWeek.Saturday)
     );
 
-    static Schedule Week() => new Schedule(
+    private static Schedule Week() => new Schedule(
         () => Logger.Information("Week: a new week has started"),
         run => run.Every(1).Weeks()
     );
 
-    static void JobStartedHandler(object sender, JobStartedEventArgs ea) =>
+    private static void JobStartedHandler(object sender, JobStartedEventArgs ea) =>
         Logger.Information("JobStarted");
 
-    static void JobEndedHandler(object sender, JobEndedEventArgs ea) =>
+    private static void JobEndedHandler(object sender, JobEndedEventArgs ea) =>
         Logger.Information(ea.Exception is null ? $"JobEnded" : $"JobEnded: {ea.Exception}");
 }
